2bb28762365d59a85ad61c13a414e8f9fa667f48
Anton Voytov <august-ra@ya.ru>
2025-02-10 14:23:01 +0300

    Fixed the last step

--- src/components/LocalMap.jsx
+++ src/components/LocalMap.jsx
@@ -1,14 +1,14 @@
 import { YMaps, Map, Clusterer, Placemark, FullscreenControl, TypeSelector, ListBox, ListBoxItem } from "@pbe/react-yandex-maps"
 
 
-export default function LocalMap() {
+export default function LocalMap({ data }) {
   return (
     <>
       <YMaps query={{ apikey: "d5867896-9ed6-4b6b-8dae-75c36596c079" }}>
         <div>
           <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} width="100vw" height="100vh">
             <FullscreenControl />
-            <TypeSelector options={{ float: "right" }} />
+            <TypeSelector />
           </Map>
         </div>
       </YMaps>
